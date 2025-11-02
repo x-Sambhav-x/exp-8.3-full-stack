78b95fd699fa3f65bd54bddd5625aa786da522e2
x-Sambhav-x <nikhrasambhav@gmail.com>
2025-11-02 23:19:30 +0530

    Delete Login.js

--- Login.js
+++
@@ -1,9 +0,0 @@
-const res = await axios.post("http://localhost:5000/api/auth/login", {
-  email,
-  password
-});
-
-localStorage.setItem("token", res.data.token);
-localStorage.setItem("role", res.data.user.role);
-
-navigate("/dashboard");
